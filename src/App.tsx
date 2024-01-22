4d3f50198a2b5e3eb7c9882fd998acff4474fd69
Menatalla Gomaa <minafathi98@gmail.com>
2024-01-22 15:11:51 +0100

    cleaning up the folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { Routes, Route } from "react-router-dom"
-import { Container } from "react-bootstrap"
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Store } from "./pages/Store";
 
-function App(){
-  return <Container>Hi</Container>
+function App() {
+  return (
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+export default App;
