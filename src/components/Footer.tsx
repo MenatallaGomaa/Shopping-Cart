92ed6d02d6925b0934c6f7db43ba01f465570a29
Menatalla Gomaa <minafathi98@gmail.com>
2024-01-27 11:01:43 +0100

    cleaning up the code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 function Footer() {
-  return <p className="bg-white shadow-sm mb-3">© 2023 All-in-One Bazar</p>;
+  return <p className="bg-white shadow-sm mb-3">© 2024 All-in-One Bazar</p>;
 }
 
 export default Footer;
